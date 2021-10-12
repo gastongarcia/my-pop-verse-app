ac95f0bb8f49018ed5060aa48ea93620175deedc
G <gaston.garcia@gmail.com>
2021-10-11 20:42:39 -0600

    Adding Google font to title. Adding animated gif. Fixed spacing on mobile view

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { ethers } from "ethers";
+import Head from "next/head";
 
 function Home() {
   const [currentAccount, setCurrentAccount] = useState("");
@@ -65,36 +66,54 @@ function Home() {
   }, []);
 
   return (
-    <div className="w-full lg:w-1/2 mx-auto m-1">
-      <h1 className="text-3xl text-white py-8">
-        Pop ETH Songs List Collection 🥤
-      </h1>
-      <p className="text-white">
-        Please send me a Youtube link with your favourite Pop song.
-      </p>
-      <p className="text-white">I'll play it at least once.</p>
-      <p className="text-white">
-        And maybe, if I like it, I'll play it non-stop.
-      </p>
+    <div>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Monoton&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <div className="w-full p-3 lg:p-0 lg:w-2/3 mx-auto m-1">
+        <h1 className="text-4xl text-white py-4 lg:py-8">
+          Pop Songs Through The Ether 🥤
+        </h1>
+        <p className="text-white mt-5">
+          Please send me a Youtube link with your favourite Pop song.
+        </p>
+        <p className="text-white">I'll play it at least once.</p>
+        <p className="text-white">
+          And maybe, if I like it, I'll play it non-stop.
+        </p>
 
-      <div className="song-form mt-8">
-        <button
-          className="w-full p-4 bg-gray-900 rounded-lg text-white hover:bg-gray-200 hover:text-gray-800 transition-all duration-500"
-          onClick={null}
-        >
-          Send my song
-        </button>
-      </div>
-
-      <div className="connect-account mt-8">
-        {!currentAccount && (
+        <div className="song-form mt-8">
           <button
-            className="waveButton bg-yellow-200 px-5 py-3 rounded-lg"
-            onClick={connectWallet}
+            className="w-full p-4 bg-gray-900 rounded-lg text-white hover:bg-gray-200 hover:text-gray-800 transition-all duration-500"
+            onClick={null}
           >
-            Connect your MetaMask Wallet
+            Send my song
           </button>
-        )}
+        </div>
+
+        <div className="connect-account mt-8">
+          {!currentAccount && (
+            <button
+              className="waveButton bg-yellow-200 px-5 py-3 rounded-lg"
+              onClick={connectWallet}
+            >
+              Connect your MetaMask Wallet
+            </button>
+          )}
+        </div>
+
+        <div className="lionel text-center mt-8">
+          <img
+            src="https://thumbs.gfycat.com/BrilliantSplendidDuiker-max-1mb.gif"
+            alt="Lionel"
+            className="mx-auto border border-gray-800"
+          />
+        </div>
       </div>
     </div>
   );
