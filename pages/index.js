aa505c3861a6b01a6c1b8e0b41e6d2ceeb5115da
G <gaston.garcia@gmail.com>
2021-10-11 21:37:24 -0600

    Added a function on load to get total songs sent.
    Added colors
    
    Added favicon

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,8 @@ import abi from "../utils/SongPortal.json";
 function Home() {
   const [currentAccount, setCurrentAccount] = useState("");
 
+  const [totalSongs, setTotalSongs] = useState(0);
+
   const contractAddress = "0x3e9d0ed6967eBB254dE6b7b8fe07386eE943eE05";
 
   const contractABI = abi.abi;
@@ -21,6 +23,17 @@ function Home() {
         return;
       } else {
         console.log("We have the ethereum object", ethereum);
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const songPortalContract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+
+        let count = await songPortalContract.getTotalSongs();
+        console.log("Retrieved total song count...", count.toNumber());
+        setTotalSongs(count.toNumber());
       }
 
       /*
@@ -51,7 +64,6 @@ function Home() {
         alert("Get MetaMask!");
         return;
       }
-
       const accounts = await ethereum.request({
         method: "eth_requestAccounts",
       });
@@ -65,7 +77,6 @@ function Home() {
 
   //the push song function - calls the function on the contract
   const song = async () => {
-    console.log("Song button called");
     try {
       const { ethereum } = window;
 
@@ -79,7 +90,20 @@ function Home() {
         );
 
         let count = await songPortalContract.getTotalSongs();
-        console.log("Retrieved total songs count...", count.toNumber());
+        console.log("Retrieved total song count...", count.toNumber());
+        setTotalSongs(count.toNumber());
+        /*
+         * Execute the actual song from your smart contract
+         */
+        const songTxn = await songPortalContract.song();
+        console.log("Mining...", songTxn.hash);
+
+        await songTxn.wait();
+        console.log("Mined -- ", songTxn.hash);
+
+        count = await songPortalContract.getTotalSongs();
+        console.log("Retrieved total song count...", count.toNumber());
+        setTotalSongs(count.toNumber());
       } else {
         console.log("Ethereum object doesn't exist!");
       }
@@ -106,30 +130,37 @@ function Home() {
         />
       </Head>
       <div className="w-full p-3 lg:p-0 lg:w-2/3 mx-auto m-1">
-        <h1 className="text-4xl text-white py-4 lg:py-8">
+        <h1 className="text-4xl text-blue-900 py-4 lg:py-8">
           Pop Songs Through The Ether 🥤
         </h1>
-        <p className="text-white mt-5">
-          Please send me a Youtube link with your favourite Pop song.
+        <p className="text-gray-900 mt-5">
+          You can send me a Pop Song using the button below.
         </p>
-        <p className="text-white">I'll play it at least once.</p>
-        <p className="text-white">
-          And maybe, if I like it, I'll play it non-stop.
+        <p className="text-gray-900">I'll play it at least once.</p>
+        <p className="text-gray-900">
+          And maybe, if I like it, I'll play it{" "}
+          <span className="font-bold">All Night Long</span>.
         </p>
+
+        {totalSongs !== 0 && (
+          <p className="text-gray-900 text-2xl mt-8">
+            Total songs sent to me so far: {totalSongs}
+          </p>
+        )}
 
         <div className="song-form mt-8">
           <button
-            className="w-full p-4 bg-gray-900 rounded-lg text-white hover:bg-gray-200 hover:text-gray-800 transition-all duration-500"
+            className="w-full lg:w-3/5 p-4 bg-gray-900 rounded-lg text-white hover:bg-gray-200 hover:text-gray-800 transition-all duration-500"
             onClick={song}
           >
-            Send my song
+            Hit me with a song
           </button>
         </div>
 
         <div className="connect-account mt-8">
           {!currentAccount && (
             <button
-              className="waveButton bg-yellow-200 px-5 py-3 rounded-lg"
+              className="songButton bg-yellow-200 px-5 py-3 rounded-lg"
               onClick={connectWallet}
             >
               Connect your MetaMask Wallet
@@ -137,11 +168,11 @@ function Home() {
           )}
         </div>
 
-        <div className="lionel text-center mt-8">
+        <div className="lionel mt-8 text-center lg:text-left">
           <img
             src="https://thumbs.gfycat.com/BrilliantSplendidDuiker-max-1mb.gif"
             alt="Lionel"
-            className="mx-auto border border-gray-800"
+            className=" mx-auto lg:ml-1 border border-gray-800"
           />
         </div>
       </div>
