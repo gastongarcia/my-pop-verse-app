1f2d5dcbeae5fd2b0ef2bfc8edfeb0df188e81b4
G <gaston.garcia@gmail.com>
2021-10-11 20:09:43 -0600

    Added code to check if MetaMask account is present. You can check it in the DevTools Console.

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,48 @@
-function Home() {
-    return (
-        <div className="w-full lg:w-1/2 mx-auto m-1">
-            <h1 className="text-3xl text-white py-8">Pop ETH Songs List Collection 🥤</h1>
-            <p className="text-white">Please send me a Youtube link with your favourite Pop song.</p>
-            <p className="text-white">I'll play it at least once.</p>
-            <p className="text-white">And maybe, if I like it, I'll play it non-stop.</p>
+import { useEffect } from "react";
+import { ethers } from "ethers";
 
-            <div className="song-form mt-8">
-                <button className="w-full p-4 bg-gray-900 rounded-lg text-white hover:bg-gray-200 hover:text-gray-800 transition-all duration-500">Send my song</button>
-            </div>
-        </div>
-    )
+function Home() {
+  const checkIfWalletIsConnected = () => {
+    /*
+     * First make sure we have access to window.ethereum
+     */
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Make sure you have metamask!");
+      return;
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+  };
+
+  /*
+   * This runs our function when the page loads.
+   */
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
+  return (
+    <div className="w-full lg:w-1/2 mx-auto m-1">
+      <h1 className="text-3xl text-white py-8">
+        Pop ETH Songs List Collection 🥤
+      </h1>
+      <p className="text-white">
+        Please send me a Youtube link with your favourite Pop song.
+      </p>
+      <p className="text-white">I'll play it at least once.</p>
+      <p className="text-white">
+        And maybe, if I like it, I'll play it non-stop.
+      </p>
+
+      <div className="song-form mt-8">
+        <button className="w-full p-4 bg-gray-900 rounded-lg text-white hover:bg-gray-200 hover:text-gray-800 transition-all duration-500">
+          Send my song
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
