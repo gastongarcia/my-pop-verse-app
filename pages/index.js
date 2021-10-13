6bbd75cdccea03edb367884c5bc42ca0e5704a0a
G <gaston.garcia@gmail.com>
2021-10-13 16:30:07 -0600

    Functions have changed. Now I fetch a song array, with messages and addresses.

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,9 @@ import abi from "../utils/SongPortal.json";
 function Home() {
   const [currentAccount, setCurrentAccount] = useState("");
 
-  const [totalSongs, setTotalSongs] = useState(0);
+  const [allSongs, setAllSongs] = useState([]);
 
-  const contractAddress = "0x3e9d0ed6967eBB254dE6b7b8fe07386eE943eE05";
+  const contractAddress = "0x0F64a3575B7E52072AB885BbBEBb14D2fB1DFc61";
 
   const contractABI = abi.abi;
 
@@ -31,9 +31,12 @@ function Home() {
           signer
         );
 
-        let count = await songPortalContract.getTotalSongs();
-        console.log("Retrieved total song count...", count.toNumber());
-        setTotalSongs(count.toNumber());
+        // let count = await songPortalContract.getTotalSongs();
+        // console.log("Retrieved total song count...", count.toNumber());
+        // setTotalSongs(count.toNumber());
+
+        console.log(`Getting all songs initially: ${allSongs.length}`);
+        getAllSongs();
       }
 
       /*
@@ -75,8 +78,49 @@ function Home() {
     }
   };
 
+  const getAllSongs = async () => {
+    console.log(`Get getAllSongs function called`);
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const songPortalContract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+
+        const songs = await songPortalContract.getAllSongs();
+        console.log(
+          `This is the songs object, called from getAllSongs function in react: ${songs}`
+        );
+
+        /*
+         * We only need address, timestamp, and message in our UI so let's
+         * pick those out
+         */
+        let songsCleaned = [];
+        songs.forEach((song) => {
+          songsCleaned.push({
+            address: song.song_sender,
+            timestamp: new Date(song.timestamp * 1000),
+            message: song.message,
+          });
+        });
+
+        setAllSongs(songsCleaned);
+      } else {
+        console.log("Ethereum object doesn't exist.");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   //the push song function - calls the function on the contract
   const song = async () => {
+    console.log(`song() function called`);
     try {
       const { ethereum } = window;
 
@@ -89,24 +133,25 @@ function Home() {
           signer
         );
 
-        let count = await songPortalContract.getTotalSongs();
-        console.log("Retrieved total song count...", count.toNumber());
-        setTotalSongs(count.toNumber());
+        let count = await songPortalContract.getAllSongs();
+        console.log("Count:", count);
+        //setAllSongs(count);
         /*
          * Execute the actual song from your smart contract
          */
-        const songTxn = await songPortalContract.song();
+        const songTxn = await songPortalContract.song("This is a message");
         console.log("Mining...", songTxn.hash);
 
         await songTxn.wait();
         console.log("Mined -- ", songTxn.hash);
 
-        count = await songPortalContract.getTotalSongs();
-        console.log("Retrieved total song count...", count.toNumber());
-        setTotalSongs(count.toNumber());
+        count = await songPortalContract.getAllSongs();
+        console.log("Retrieved total song count...", count);
+        setAllSongs(count);
       } else {
         console.log("Ethereum object doesn't exist!");
       }
+      getAllSongs();
     } catch (error) {
       console.log(error);
     }
@@ -142,9 +187,9 @@ function Home() {
           <span className="font-bold">All Night Long</span>.
         </p>
 
-        {totalSongs !== 0 && (
+        {allSongs !== null && (
           <p className="text-gray-900 text-2xl mt-8">
-            Total songs sent to me so far: {totalSongs}
+            Total songs sent to me so far: {allSongs.length}
           </p>
         )}
 
@@ -166,6 +211,18 @@ function Home() {
               Connect your MetaMask Wallet
             </button>
           )}
+        </div>
+
+        <div className="all-waves">
+          {allSongs.map((song, index) => {
+            return (
+              <div key={index}>
+                <p>
+                  {song.address}, {song.timestamp.toString()}, {song.message}
+                </p>
+              </div>
+            );
+          })}
         </div>
 
         <div className="lionel mt-8 text-center lg:text-left">
